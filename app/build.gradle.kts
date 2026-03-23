a356dffaabddeb4cb81b92686c3b2308d4f9bc47
woals <woals6318@gmail.com>
2026-03-24 02:50:44 +0900

    edit build.gradle package name com.exmaple.myapplication to com.tasty.android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.tasty.android"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.tasty.android"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
