a205a45d724bf2ab7e4aa961dcb8f0fb7bfe5bab
JungSub <ljs990326@gmail.com>
2026-03-24 14:44:07 +0900

    뷰 모델 수정, mapper + 파이어스토어 컬렉션 데이터 구조화(feeds 부분만)

---
+++ app/src/main/java/com/tasty/android/core/model/FeedComment.kt
@@ -0,0 +1,9 @@
+package com.tasty.android.core.model
+
+data class FeedComment(
+    val commentId: String = "",
+    val feedId: String = "",
+    val authorId: String = "",
+    val content: String = "",
+    val createdAt: String = ""
+)
