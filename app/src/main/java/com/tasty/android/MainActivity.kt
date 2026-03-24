5cc31a8ecf2d25dfa2a473586ab4403186357d39
woals <woals6318@gmail.com>
2026-03-25 04:32:33 +0900

    edit MainActiity to define CustomScaffold, edit file name  FeedViewmodel -> FeedViewModel

--- app/src/main/java/com/tasty/android/MainActivity.kt
+++ app/src/main/java/com/tasty/android/MainActivity.kt
@@ -6,28 +6,37 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Send
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.compose.rememberNavController
+import com.tasty.android.core.design.component.AppBarAction
+import com.tasty.android.core.design.component.CustomTopAppBar
 import com.tasty.android.core.design.theme.MyApplicationTheme
 import com.tasty.android.core.navigation.CustomNavHost
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
                 val navController = rememberNavController() // NavController 선언
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CustomNavHost( // CustomNavHost 선언
-                        navController = navController,
-                        modifier = Modifier.padding(innerPadding)
+                        navController = navController
                     )
-                }
+
             }
         }
     }
 }
+
