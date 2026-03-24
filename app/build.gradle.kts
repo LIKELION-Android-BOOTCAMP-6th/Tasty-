aeb1ebea6e8dbd887d9dadc507e11d79d3894047
woals <woals6318@gmail.com>
2026-03-24 10:20:27 +0900

    add icon dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-text-google-fonts:1.10.5")
     // Add google Icons Dependency (아이콘 확장 라이브러리 추가)
     implementation("androidx.compose.material:material-icons-extended")
+    // Add google Icons Dependency (아이콘 라이브러리 추가)
+    implementation("androidx.compose.material:material-icons-core")
 }
