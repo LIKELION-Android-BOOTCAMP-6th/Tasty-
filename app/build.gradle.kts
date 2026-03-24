36f9bc91b166a1b42f13d59de0b6bd9608c37132
woals <woals6318@gmail.com>
2026-03-24 09:09:41 +0900

    Chore/add icon assets dependency (#7)
    * Add the dependency for the Coil Lib
    
    * add Google Font dependency
    
    * add font_certs.xml and Re-declare Typo, Color, Theme
    
    * add-component-package-and-Appbar Files
    
    * add icon assets dependency
    
    * edited-add icon assets dependency
    
    ---------
    
    Co-authored-by: AndroidJam <woals6318@hufs.ac.kr>
    
    구글 머테리얼 3 아이콘 확장 라이브러리 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,8 @@ dependencies {
     // Add the dependency for the Coil Lib
     implementation("io.coil-kt.coil3:coil-compose:3.4.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
-
+    // Add the dependency for Google Dependency
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.10.5")
+    // Add google Icons Dependency (아이콘 확장 라이브러리 추가)
+    implementation("androidx.compose.material:material-icons-extended")
 }
--- app/src/main/java/com/tasty/android/core/design/theme/Type.kt
+++ app/src/main/java/com/tasty/android/core/design/theme/Type.kt
@@ -1,34 +1,31 @@
 package com.tasty.android.core.design.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.tasty.android.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+// Declare Google Font Provider
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+// Declare fontName: Roboto
+val fontName = GoogleFont("Roboto")
+
+// Declare fontFamily
+val fontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Light), // W300
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Normal), // W400
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Medium), // W500
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.SemiBold), // W600
+    Font(googleFont = fontName, fontProvider = provider, weight = FontWeight.Bold), // W700
 )
