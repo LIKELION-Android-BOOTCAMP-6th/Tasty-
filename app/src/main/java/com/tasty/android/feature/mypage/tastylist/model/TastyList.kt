850a1940c3751356f3657c37e4f05b58a38445f3
박재민1 <woals6318@gmail.com>
2026-03-30 03:32:18 +0900

    Core/impl tasty list store (#58)
    * commit dummy
    
    * implement tastyList FirestoreManager
    
    * 파이어스토어 기능구현 완료 및 필요한 유틸 함수 작성, 필요한 모델 수정 및 추가

--- app/src/main/java/com/tasty/android/feature/mypage/tastylist/model/TastyList.kt
+++ app/src/main/java/com/tasty/android/feature/mypage/tastylist/model/TastyList.kt
@@ -1,12 +1,21 @@
-package com.tasty.android.feature.tastylist.model
+package com.tasty.android.feature.mypage.tastylist.model
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ServerTimestamp
+import com.tasty.android.core.model.UserSummary
+import com.tasty.android.feature.feed.model.Feed
 
 data class TastyList(
-    val tastyListId: String = "",
-    val authorId: String = "",
-    val title: String = "",
-    val thumbnailImageUrl: String = "",
-    val likeCount: Int = 0,
-    val viewCount: Int = 0,
-    val createdAt: String,
-    val updatedAt: String
+    val tastyListId: String = "",                        // Tasty 리스트 고유 ID
+    val title: String = "",                            // 리스트 제목
+    val thumbnailImageUrl: String? = "",                   // 썸네일 이미지 URL
+    @ServerTimestamp
+    val createdAt: Timestamp? = null,                        // 생성일 
+    @ServerTimestamp
+    val updatedAt: Timestamp? = null,
+    val feedIds: List<String> = emptyList(), // 포함된 피드 아이디들
+    val authorId: String = "",        // 작성 유저 ID
+
+    val likeCount: Int = 0,   // 좋아요 수
+    val viewCount: Int = 0 // 조회수
 )
