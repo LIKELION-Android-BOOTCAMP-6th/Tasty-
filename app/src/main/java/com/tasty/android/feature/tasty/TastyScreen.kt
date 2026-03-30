c81190252eff04eff1d41be9127641feec23a4e8
JungSub <ljs990326@gmail.com>
2026-03-30 09:02:25 +0900

    마이페이지- 테이스티리스트

--- app/src/main/java/com/tasty/android/feature/tasty/TastyScreen.kt
+++ app/src/main/java/com/tasty/android/feature/tasty/TastyScreen.kt
@@ -1,4 +1,4 @@
-package com.tasty.android.feature.tastylist
+package com.tasty.android.feature.tasty
 
 import androidx.compose.runtime.Composable
 import com.tasty.android.core.design.component.ScaffoldConfig
